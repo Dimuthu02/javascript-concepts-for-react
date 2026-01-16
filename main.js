a2a42ef945642ebe26cd5f217e5435859f490d69
Dimuthu <Sanjuladimuthu2@gmail.com>
2026-01-15 22:58:25 -0800

    Refactor main.js to add getage and getcity functions with conditional logging

--- main.js
+++ main.js
@@ -2,8 +2,34 @@ function getname()
 {
     return 'my name is Dimuthu'
 }
-
 let a=true
 let b=false
-
 console.log(a || getname());
+
+//Ternery operator
+
+let isbin=true
+
+function getage(age)
+{
+    return `my age is ${age} old.`
+}
+
+function getcity(city)
+{
+return ` iam from ${city}`
+}
+
+if(isbin)
+{
+    console.log(getage(22));
+    
+}
+else{
+    console.log(getcity("Galle"));
+    
+}
+
+//this is ternery operator.it can get output using one line
+
+isbin ? console.log(getage(22)) : console.log(getcity("Galle"));
